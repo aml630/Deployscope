b12abefa9c77cab89dd9da03c3af97e885e84aeb
alex <alex.m.larson@gmail.com>
2016-02-05 18:02:56 -0800

    got kadira working and spaced sidebar

---
+++ client/blog.js
@@ -0,0 +1,9 @@
+// Template.blog.onCreated(function () {
+//   Telescope.SEO.setTitle(Pages.findOne({slug: FlowRouter.getParam("slug")}).title);
+// });
+//
+// Template.page.helpers({
+//   page: function () {
+//     return Pages.findOne({slug: FlowRouter.getParam("slug")});
+//   }
+// });
---
+++ server/kadira.js
@@ -0,0 +1,1 @@
+Kadira.connect('v3PrpJFxnBA8z3RZt', 'c0345094-caed-4f19-b44e-19270d8f0000');
